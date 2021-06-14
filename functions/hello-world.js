3b656910b70179cc336169b6202ccdcd4dd9b7e0
rlajous <rodrigo@lajous.com.ar>
2021-06-14 11:19:28 -0300

    Fixing

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -68,31 +68,7 @@ router.get('/', (req, res) => {
     </html>`);
     res.end();
   } else {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(`
-    <!doctype html>
-    <head>
-          <title>hola</title>
-          <meta property="og:type" content="article">
-          <meta property="og:site_name" content="Quipu Market">
-          <meta property="og:title" content="$hola">
-          <meta property="og:description" content="hola">
-          <meta property="og:image:height" content="200">
-          <meta property="og:image:width" content="300">
-    </head>
-    <body>
-      <article>
-        <div>
-          <h2>hola humano</h2>
-        </div>
-        <div>
-          <p>hola</p>
-        </div>
-      </article>
-    </body>
-    </html>`);
-    res.end();
-    //res.redirect('http://'+req.hostname+'/r'+req.originalUrl)
+    res.redirect('http://'+req.hostname+'/r'+req.originalUrl)
   }
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
