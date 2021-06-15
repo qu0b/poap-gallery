3052f1ba777a4b49c1edcef66e1876be6bac4ad6
rlajous <rodrigo@lajous.com.ar>
2021-06-15 10:53:24 -0300

    PATH fix

--- netlify/functions/render.js
+++ netlify/functions/render.js
@@ -52,31 +52,36 @@ router.get('/', async (req, res) => {
   if (isBot) {
     const event = await getEvent()
     const { data } = event
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(`
-    <!doctype html>
-    <head>
-          <title>hola</title>
-          <meta property="og:type" content="article">
-          <meta property="og:site_name" content="POAP Gallery">
-          <meta property="og:title" content="${data.name}">
-          <meta property="og:description" content="${data.description}">
-          <meta property="og:image" content="${data.image_url}">
-          <meta property="og:image:height" content="200">
-          <meta property="og:image:width" content="200">
-    </head>
-    <body>
-      <article>
-        <div>
-          <h2>hola</h2>
-        </div>
-        <div>
-          <p>hola</p>
-        </div>
-      </article>
-    </body>
-    </html>`);
-    res.end();
+    if (data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`
+      <!doctype html>
+      <head>
+            <title>hola</title>
+            <meta property="og:type" content="article">
+            <meta property="og:site_name" content="POAP Gallery">
+            <meta property="og:title" content="${data.name}">
+            <meta property="og:description" content="${data.description}">
+            <meta property="og:image" content="${data.image_url}">
+            <meta property="og:image:height" content="200">
+            <meta property="og:image:width" content="200">
+      </head>
+      <body>
+        <article>
+          <div>
+            <h2>hola</h2>
+          </div>
+          <div>
+            <p>hola</p>
+          </div>
+        </article>
+      </body>
+      </html>`);
+      res.end();
+    } else {
+      res.redirect('http://'+req.hostname)
+    }
+    
   } else {
     res.redirect('http://'+req.hostname+'/r/event/'+ req.baseUrl.split('/')[4])
   }
