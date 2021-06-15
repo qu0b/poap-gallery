5fdcbbe579cc5dac3954dc0f5b0e3e2e8ba6f309
rlajous <rodrigo@lajous.com.ar>
2021-06-15 13:40:42 -0300

    PATH fix

--- netlify/functions/render.js
+++ netlify/functions/render.js
@@ -95,7 +95,7 @@ app.use(morgan(function (tokens, req, res) {
     tokens['response-time'](req, res), 'ms'
   ].join(' ')
 }))
-app.use(['/.netlify/functions/render/*', '/.netlify/functions/render/','/.netlify/functions/render/event/*', '/'], router);  // path must route to lambda
+app.use(['/.netlify/functions/render/*', '/.netlify/functions/render/','/.netlify/functions/render/event/*', '/event/*'], router);  // path must route to lambda
 
 module.exports.handler = serverless(app);
 
