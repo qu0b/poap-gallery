9bd534b67753bf3f2fa075cd50e989c36c1674f4
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2022-11-04 09:29:20 -0500

    ✨Add: POAP API API-Key (#115)
    * ✨Add: POAP API API-Key
    
    * ♻️ Refactor: abstract API-Key into one single function

--- src/store/api.js
+++ src/store/api.js
@@ -1,6 +1,7 @@
 export const XDAI_SUBGRAPH_URL = process.env.REACT_APP_XDAI_SUBGRAPH_URL;
 export const MAINNET_SUBGRAPH_URL = process.env.REACT_APP_MAINNET_SUBGRAPH_URL;
 export const POAP_API_URL = process.env.REACT_APP_POAP_API_URL;
+export const POAP_API_API_KEY = process.env.REACT_APP_POAP_API_API_KEY;
 export const POAP_APP_URL = process.env.REACT_APP_POAP_APP_URL;
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const OrderType = {
@@ -38,36 +39,27 @@ export const OrderDirection = {
 
 export const PAGE_LIMIT = 20
 
-export async function getPaginatedEvents({name = undefined, event_ids = undefined,
-                                           offset = undefined, limit = undefined,
-                                           orderBy = undefined, privateEvents = undefined}) {
-  const url = new URL(`${POAP_API_URL}/paginated-events`)
-  if (name) {
-    url.searchParams.append('name', name)
-  }
-  if (event_ids && event_ids.length) {
-    url.searchParams.append('event_ids', event_ids)
-  }
-  if (limit && limit > 0) {
-    url.searchParams.append('limit', limit)
-  }
-  if (offset !== undefined && offset >= 0) {
-    url.searchParams.append('offset', offset)
-  }
-  if (orderBy?.type && orderBy?.order) {
-    url.searchParams.append('sort_field', orderBy.type)
-    url.searchParams.append('sort_dir', orderBy.order)
-  }
-  if (privateEvents !== undefined) {
-    url.searchParams.append('private_event', privateEvents)
-  }
-  const res = await fetch(url.href)
-  return res.json()
+export async function getPaginatedEvents({
+                                             name = undefined,
+                                             event_ids = undefined,
+                                             offset = undefined,
+                                             limit = undefined,
+                                             orderBy = undefined,
+                                             privateEvents = undefined
+                                         }) {
+    let queryParams = {
+        name, event_ids, limit, offset, private_event: privateEvents,
+    };
+
+    if (orderBy?.type && orderBy?.order) {
+        queryParams = {...queryParams, sort_field: orderBy.type, sort_dir: orderBy.order};
+    }
+
+    return await fetchPOAPApi('/paginated-events', queryParams);
 }
 
 export async function getEvent(id) {
-  const res = await fetch(`${POAP_API_URL}/events/id/${id}`)
-  return res.json()
+    return await fetchPOAPApi(`/events/id/${id}`);
 }
 
 export async function getLayerEvents(url, first, skip, orderBy) {
@@ -308,6 +300,41 @@ export async function validateMigrations(migrations) {
 }
 
 export async function getTop3Events() {
-  const res = await fetch(`${POAP_API_URL}/top-3-events`)
-  return res.json()
+  return await fetchPOAPApi('/top-3-events');
+}
+
+function setQueryParamsToUrl(url, queryParams) {
+    if (!queryParams) {
+        return;
+    }
+
+    for (const key in queryParams) {
+        const value = queryParams[key];
+
+        if (value === undefined) {
+            continue;
+        }
+
+        url.searchParams.append(key, value);
+    }
+}
+
+function buildPOAPApiHeaders(init) {
+    const headers = {'X-API-Key': POAP_API_API_KEY};
+
+    if (!init || !init.headers) {
+        return headers;
+    }
+
+    return {...(init.headers), ...headers};
+}
+
+async function fetchPOAPApi(path, queryParams, init) {
+    const url = new URL(`${POAP_API_URL}${path}`);
+    const headers = buildPOAPApiHeaders(init);
+
+    setQueryParamsToUrl(url, queryParams);
+
+    const res = await fetch(url, {headers});
+    return res.json();
 }
